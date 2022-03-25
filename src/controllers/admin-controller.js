78f44dc0ab0497a9eb3a799567b245c5019516ca
Margaret McCarthy <76971580+Magsers@users.noreply.github.com>
2022-03-25 18:53:07 +0000

    Sort Routes by Grade attempt
    Incomplete

--- src/controllers/admin-controller.js
+++ src/controllers/admin-controller.js
@@ -7,12 +7,11 @@ export const adminController = {
         const users = await db.userStore.getAllUsers();
         const crags = await db.cragStore.getAllCrags();
         const routes = await db.routeStore.getAllRoutes();
-        const usercrags = await db.cragStore.getUserCrags(request.params.id).length;
         const viewData = {
           title: "Placemark Admin Dashboard",
           user: loggedInUser,
           users: users,
-          usercrags: usercrags,
+          usercrags: users.crags.length,
           cragCount: crags.length,
           routeCount: routes.length,
         };
